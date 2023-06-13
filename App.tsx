4f9af3bb75c41eeaa3fdce2bd73a4adaa919b7d2
Stephan D Ramalho <sramalho@fordham.edu>
2023-06-13 15:18:23 -0400

    small tweaks before meeting

--- App.tsx
+++ App.tsx
@@ -1,18 +1,19 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
 import React from 'react';
-
+import {NavigationContainer} from '@react-navigation/native';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import {SafeAreaView, Text, View} from 'react-native';
+import Home from './src/screens/Home';
 
+export type RootStackParamList = {
+  Home: undefined;
+  Login: undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 function App(): JSX.Element {
   return (
-    <SafeAreaView>
-      <Text className="text-red-500">App.tsx</Text>
+    <SafeAreaView className="flex-row justify-center">
+      <Text className="text-orange-600">Set up Navigation here</Text>
     </SafeAreaView>
   );
 }
