e77837da8cd67e5576aad8ebe7a1fa49ff604945
Stephan D Ramalho <sramalho@fordham.edu>
2023-06-14 12:41:17 -0400

    added logout button

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,10 +1,23 @@
-import {View, Text} from 'react-native';
 import React from 'react';
+import {View, Text, Button} from 'react-native';
+import {useAuth} from '../../app/context/AuthContext'; // update this path if needed
 
 const Home = () => {
+  const {onLogout} = useAuth();
+  // get the onLogout function from AuthContext
+
+  const handleLogout = async () => {
+    try {
+      await onLogout(); // call the onLogout function when the button is pressed
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
   return (
     <View>
       <Text>Home</Text>
+      <Button title="Logout" onPress={handleLogout} />
     </View>
   );
 };
